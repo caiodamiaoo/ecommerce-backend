404db334e040298a11795e1581618a1252799c76
Caio Damiao <flongcd@gmail.com>
2025-07-31 23:53:28 -0300

    feat: add anotacao para habilitar o feign

--- src/main/java/com/flong/ecommerce/EcommerceBackendApplication.java
+++ src/main/java/com/flong/ecommerce/EcommerceBackendApplication.java
@@ -2,12 +2,14 @@ package com.flong.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class EcommerceBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
 
 }
